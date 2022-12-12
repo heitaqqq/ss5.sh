20b9a6c0cf85543d6caa9b83ee2a9a68c02ee2b6
heitaqqq <897333244@qq.com>
2022-12-12 14:09:18 +0800

    Update ss5.sh

--- ss5.sh
+++ ss5.sh
@@ -118,9 +118,9 @@ bbr_install() {
 
 user_set() {
 	#read -rp  "请设置ss5账户。默认:admin）:" user
-	[[ -z ${user} ]] && user="aa852123.."
+	[[ -z ${user} ]] && user="ruoji123"
 	#read -rp "请设置ss5连接密码。默认:admin）:" passwd
-	[[ -z ${passwd} ]] && passwd="aa852123.."
+	[[ -z ${passwd} ]] && passwd="ruoji123"
 }
 
 install_ss5() {
